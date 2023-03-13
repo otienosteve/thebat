118a2ca3430311d02c7308e739ab8c7cb055098b
otienosteve <steveotieno701@gmail.com>
2023-03-13 12:02:25 +0300

    Change Header

--- data.js
+++ data.js
@@ -1,40 +1,63 @@
+
 var admin = require("firebase-admin");
 var clusters=[],bats=[],sites=[],enumarator=[],geoloc=[]
 var serviceAccount = require("./conservation-programme-firebase-adminsdk-s1tmf-7e9429afa6.json");
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-  //   The database URL depends on the location of the database
+     //   The database URL depends on the location of the database
     databaseURL: "https://conservation-programme-default-rtdb.firebaseio.com/"
   });
  
 pr=console.log
-var db = admin.database();
-var ref = db.ref("/data/");
-ref.once("value", async function(snap) {
-   snap.forEach(function(childNodes){
-        let keys=Object.keys(childNodes.val())
 
-    let cord=[]
-    for( let i of keys){
-        
-    clusters=childNodes.val()[i]['clusters']
-        pr(childNodes.val()[i]['clusters'].map(n=>+n).reduce((a,v)=>a+=v)) //numBats:
-        bats.push(childNodes.val()[i]['numBats'])
-        sites.push(childNodes.val()[i]['site'])
-        cord.push({
-            latitude:childNodes.val()[i]['latitude'],
-            longitude:childNodes.val()[i]['longitude']
-        })
-    enumarator.push(childNodes.val()[i]["enumerator_name"])
+//  function returnData(){
 
-    }
-    geoloc.push(cord)
-    pr(bats)
-    pr(cord)
-    module.exports= {clusters, bats,sites,enumarator,geoloc}
-        })
-        // pr(sites)
-    });
+    var db = admin.database();
+    var ref = db.ref("/data/");
+    
+  let data=ref.once("value")
+  .then(snapshot => {
+    //   console.log("Passed value ", snapshot.val());
+      return { data: snapshot.val()}
+  })
+  .catch(error => {
+    console.log("ERROR:\n" + error);
+    return { teams: [] };
+  });
+console.log(Promise.resolve(data).then(data=>console.log(data)))
+  
 
+      
+    
+  
+//   function(snap) {
+        // return snap.forEach(function(childNodes){
+            // var keys=Object.keys(childNodes.val())
+            // nodes= childNodes.val()
+            // return nodes
+            // var cord=[]
+            // for( let i of keys){
+                // console.log(childNodes.val())
+                // clusters.push(childNodes.val()[i]['clusters'])
+                //     pr(childNodes.val()[i]['clusters'].map(n=>+n).reduce((a,v)=>a+=v)) //numBats:
+                //     bats.push(childNodes.val()[i]['numBats'])
+                //     sites.push(childNodes.val()[i]['site'])
+                //     cord.push({
+                    //         latitude:childNodes.val()[i]['latitude'],
+                    //         longitude:childNodes.val()[i]['longitude']
+                    //     })
+                    // enumarator.push(childNodes.val()[i]["enumerator_name"])
+                    
+                    // }
+                    // geoloc.push(cord)
+                    // pr(bats)
+                    // pr(cord)
+                
+                
+            // })
+        // })
+            // }
+// pr(returnData())
+module.exports= {clusters, bats,sites,enumarator,geoloc}
 
 
--- server.js
+++ server.js
@@ -1,15 +1,72 @@
 
 const express=require('express')
-const { enumarator,sites,geoloc,bats,clusters } = require('./data')
 const app= express()
-
+var admin = require("firebase-admin");
+var clusters=[],bats=[],sites=[],enumarators=[],geoloc=[]
+var serviceAccount = require("./conservation-programme-firebase-adminsdk-s1tmf-7e9429afa6.json");
 app.set('view engine', 'ejs')
-app.listen(4000)
-app.get('/',(req,res)=>{
-    res.render('index',{sites:sites})
+                app.listen(3000)
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+     //   The database URL depends on the location of the database
+    databaseURL: "https://conservation-programme-default-rtdb.firebaseio.com/"
+  });
+  pr=console.log
+  var db = admin.database();
+  var ref = db.ref("/data/");
+  ref.once("value",function(snap) {
+     snap.forEach(function(childNodes){
+        var keys=Object.keys(childNodes.val())
+         
+        var cord=[]
+        for( let i of keys){
+            console.log(childNodes.val())
+            clusters.push(childNodes.val()[i]['clusters'])
+                pr(childNodes.val()[i]['clusters'].map(n=>+n).reduce((a,v)=>a+=v)) //numBats:
+                bats.push(childNodes.val()[i]['numBats'])
+                sites.push(childNodes.val()[i]['site'])
+                cord.push({
+                        latitude:childNodes.val()[i]['latitude'],
+                        longitude:childNodes.val()[i]['longitude']
+                    })
+                enumarators.push(childNodes.val()[i]["enumerator_name"])
+                
+                }
+                let site=[...new Set(sites)]
+                
+                geoloc.push(cord)
+                pr(bats)
+                pr(cord)
+                
+                app.get('/',(req,res)=>{
+                  
+                    res.render('index',{site:site,enumarators:enumarators,bats:bats})
+                
+                })
+                app.get('/bats/',(req,res)=>{
+                bats=bats.map(n=>+n).reduce((a,v)=>a+=v)
+                    res.render('bats',{bats:bats})
+                })
+                app.get('/clusters',(req,res)=>{
+                
+                res.render('clusters')
+                })
+                app.get('/enumarators',(req,res)=>{
+                res.render('enumarators',{enumerator:enumarator})
+                
+                
+                })
+                app.get('/sites',(req,res)=>{
+                
+                res.render('sites',{site:site})
 
-})
-console.log(sites)
+                })
+            
+        })
+    })
+
+
+
 
 
 
